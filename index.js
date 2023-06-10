6b36d65affed32e156daaebab2289c222a7eeb93
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-10 16:20:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names) {
+    let result = [];
+    result.length = names.length;
+    for (let x=0;x<names.length;x++) {
+      result[x]=`Thank you, ${names[x]}, for the wonderful surprise gift!`;
+    }
+    return result;
+}
+
+function countDown(number){
+    for (let x=number;x>=0;x--) {
+        console.log(x);       
+    }
+}
